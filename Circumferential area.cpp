dc8ab4e46316e243df0160a12a3a1cf13a84eeb3
TamtaTandashvili <116017247+TamtaTandashvili@users.noreply.github.com>
2022-11-30 21:33:05 +0100

    Add files via upload

---
+++ Circumferential area.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double radius, area;
+    cout<<"შეიყვანე წრეწირის რადიუსი:";
+    cin>>radius;
+    area=3.14*radius*radius;
+    cout<<"წრეწირის ფართობი არის:";
+    cout<<area;
+
+     return 0;
+}
